84e50c1b96869b668f77e87cc61255e4e756cdf5
Junko Werner <aliciajace@gmail.com>
2015-10-29 18:39:58 -0700

    updated css, image, map

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Groomer
     # On Heroku, you must set this line in your config/application.rb:
     config.assets.initialize_on_precompile = true
 
-    Rails.application.config.assets.precompile += %w(creative.css font-awesome.css font-awesome.min.css animate.min.css css.css animate.min.css application.scss appointments.scss cdd(1) dogs.scss font-awesome.min.css welcome.scss *.scss)
-    Rails.application.config.assets.precompile += %w( *.js doggie.jpg)
+    Rails.application.config.assets.precompile += %w(creative.css.scss.erb font-awesome.css font-awesome.min.css animate.min.css css.css animate.min.css application.scss appointments.scss cdd(1) dogs.scss font-awesome.min.css welcome.scss *.scss)
+    Rails.application.config.assets.precompile += %w( *.js doggie.jpg shampoo.jpg *.jpg)
   end
 end
