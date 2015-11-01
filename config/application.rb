93205c11333a186a877bd724d18c8e358f06e212
Junko Werner <aliciajace@gmail.com>
2015-11-01 07:54:40 -0800

    awesome fonts

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module Groomer
     config.assets.initialize_on_precompile = true
 
     Rails.application.config.assets.precompile += %w(creative.css.scss.erb creative.css font-awesome.css font-awesome.min.css animate.min.css css.css animate.min.css application.scss appointments.scss cdd(1) dogs.scss font-awesome.min.css welcome.scss *.scss)
-    Rails.application.config.assets.precompile += %w( *.js doggie.jpg shampoo.jpg *.jpg *.woff *.tff)
+    Rails.application.config.assets.precompile += %w( *.js doggie.jpg shampoo.jpg *.jpg *.woff fontawesome-webfont.ttf *.ttf)
  end
 end
