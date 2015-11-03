c1e5f8e49810393ea65c8846dd654129c3307817
Junko Werner <aliciajace@gmail.com>
2015-11-02 21:29:25 -0800

    precompile files update

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,3 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w(creative.css.scss.erb creative.css font-awesome.css font-awesome.min.css animate.min.css css.css animate.min.css application.scss appointments.scss cdd(1) dogs.scss font-awesome.min.css welcome.scss *.scss)
-Rails.application.config.assets.precompile += %w( *.js doggie.jpg shampoo.jpg *.jpg *.woff *.tff)
